08a78b5fe165e6b87424d089df003d153357bc54
lovemefan <lovemefan@outlook.com>
2023-10-04 21:35:54 +0800

    defined paraformer decoder structure

--- backend/ggml/module/decoder.h
+++ backend/ggml/module/decoder.h
@@ -10,55 +10,56 @@
 #include "hparams.h"
 // token decoding layer
 struct paraformer_layer_decoder {
-    // decoder.blocks.*.attn_ln
-    struct ggml_tensor * attn_ln_0_w;
-    struct ggml_tensor * attn_ln_0_b;
 
-    // decoder.blocks.*.attn.out
-    struct ggml_tensor * attn_ln_1_w;
-    struct ggml_tensor * attn_ln_1_b;
+    // decoder.embed.0.weight
+    struct ggml_tensor * d_emb;
+    // decoder.after_norm.weight
+    struct ggml_tensor * d_norm_w;
+    struct ggml_tensor * d_norm_b;
 
-    // decoder.blocks.*.attn.query
-    struct ggml_tensor * attn_q_w;
-    struct ggml_tensor * attn_q_b;
+    // decoder.output_layer.weight
+    struct ggml_tensor * d_ln_out_w;
+    struct ggml_tensor * d_ln_out_b;
 
-    // decoder.blocks.*.attn.key
-    struct ggml_tensor * attn_k_w;
+    // decoder.self_attn.fsmn_block.weight
+    struct ggml_tensor * d_attn_fsmn_w;
+    struct ggml_tensor * d_attn_fsmn_b;
 
-    // decoder.blocks.*.attn.value
-    struct ggml_tensor * attn_v_w;
-    struct ggml_tensor * attn_v_b;
+    // decoder.src_attn.linear_q.weight
+    struct ggml_tensor * d_src_attn_ln_q_w;
+    struct ggml_tensor * d_src_attn_ln_q_b;
 
-    // decoder.blocks.*.cross_attn_ln
-    struct ggml_tensor * cross_attn_ln_0_w;
-    struct ggml_tensor * cross_attn_ln_0_b;
+    // decoder.src_attn.linear_k_v.weight
+    struct ggml_tensor * d_src_attn_ln_kv_w;
+    struct ggml_tensor * d_src_attn_ln_kv_b;
 
-    // decoder.blocks.*.cross_attn.out
-    struct ggml_tensor * cross_attn_ln_1_w;
-    struct ggml_tensor * cross_attn_ln_1_b;
+    // decoder.src_attn.linear_out.weight
+    struct ggml_tensor * d_src_attn_ln_o_w;
+    struct ggml_tensor * d_src_attn_ln_o_b;
 
-    // decoder.blocks.*.cross_attn.query
-    struct ggml_tensor * cross_attn_q_w;
-    struct ggml_tensor * cross_attn_q_b;
 
-    // decoder.blocks.*.cross_attn.key
-    struct ggml_tensor * cross_attn_k_w;
+    // decoder.feed_forward.w_1.weight
+    struct ggml_tensor * d_mlp_ln_w1;
+    struct ggml_tensor * d_mlp_ln_b1;
 
-    // decoder.blocks.*.cross_attn.value
-    struct ggml_tensor * cross_attn_v_w;
-    struct ggml_tensor * cross_attn_v_b;
+    // decoder.feed_forward.w_2.weight
+    struct ggml_tensor * d_mlp_ln_w2;
 
-    // decoder.blocks.*.mlp_ln
-    struct ggml_tensor * mlp_ln_w;
-    struct ggml_tensor * mlp_ln_b;
+    // decoder.feed_forward.norm.weight
+    struct ggml_tensor * d_mlp_norm_w;
+    struct ggml_tensor * d_mlp_norm_b;
 
-    // decoder.blocks.*.mlp.0
-    struct ggml_tensor * mlp_0_w;
-    struct ggml_tensor * mlp_0_b;
+    //decoder.norm1.weight
+    struct ggml_tensor * d_norm_w1;
+    struct ggml_tensor * d_norm_b1;
 
-    // decoder.blocks.*.mlp.2
-    struct ggml_tensor * mlp_1_w;
-    struct ggml_tensor * mlp_1_b;
+    //decoder.norm2.weight
+    struct ggml_tensor * d_norm_w2;
+    struct ggml_tensor * d_norm_b2;
+
+    //decoder.norm3.weight
+    struct ggml_tensor * d_norm_w3;
+    struct ggml_tensor * d_norm_b3;
 };
 
 
@@ -95,24 +96,89 @@ struct paraformer_sequence {
 
 // TAGS: PARAFORMER_DECODER_INIT
 struct paraformer_decoder {
-    // each decoder keeps its own KV-cache
-    paraformer_kv_cache kv_self;
 
-    // the currently generated sequence of tokens
-    paraformer_sequence sequence;
+    std::vector<paraformer_layer_decoder> decoder_layers;
+    //-------decoder.decoder3.bias_decoder------
 
-    int seek_delta; // the window shift found so far based on the decoded timestamp tokens
+    // decoder.decoder3.feed_forward.w_1.weight
+    struct ggml_tensor * d3_mlp_ln_w1;
+    struct ggml_tensor * d3_mlp_ln_b1;
 
-    bool failed;    // has the current segment failed to decode?
-    bool completed; // has the decoder completed the current segment?
-    bool has_ts;    // have we already sampled a non-beg timestamp token for the current segment?
+    // decoder.decoder3.feed_forward.w_2.weight
+    struct ggml_tensor * d3_mlp_ln_w2;
 
-    // new token probs, logits and logprobs after the last paraformer_decode (1-dimensional array: [n_vocab])
-    std::vector<float> probs;
-    std::vector<float> logits;
-    std::vector<float> logprobs;
+    // decoder.decoder3.feed_forward.norm.weight
+    struct ggml_tensor * d3_mlp_norm_w;
+    struct ggml_tensor * d3_mlp_norm_b;
 
-    std::vector<paraformer_token> tokens_tmp; // used for paraformer_decode calls
+    // decoder.decoder3.norm1.weight
+    struct ggml_tensor * d3_norm_w1;
+    struct ggml_tensor * d3_norm_b1;
+
+    //-------decoder.bias_decoder------
+
+    // decoder.bias_decoder.src_attn.linear_q.weight
+    struct ggml_tensor * d_bias_src_attn_ln_q_w;
+    struct ggml_tensor * d_bias_src_attn_ln_q_b;
+
+    // decoder.bias_decoder.src_attn.linear_k_v.weight
+    struct ggml_tensor * d_bias_src_attn_ln_kv_w;
+    struct ggml_tensor * d_bias_src_attn_ln_kv_b;
+
+    // decoder.bias_decoder.src_attn.linear_out.weight
+    struct ggml_tensor * d_bias_src_attn_ln_o_w;
+    struct ggml_tensor * d_bias_src_attn_ln_o_b;
+
+    // decoder.bias_decoder.norm3.weight
+    struct ggml_tensor * d_bias_norm_w3;
+    struct ggml_tensor * d_bias_norm_b3;
+
+    // decoder.bias_output.weight
+    struct ggml_tensor * d_bias_ln_o_w;
+    struct ggml_tensor * d_bias_ln_o_b;
+
+    //-------last_decoder.bias_decoder------
+
+    // decoder.last_decoder.self_attn.fsmn_block.weight
+    struct ggml_tensor * d_last_attn_fsmn_w;
+    struct ggml_tensor * d_last_attn_fsmn_b;
+
+    // decoder.last_decoder.src_attn.linear_q.weight
+    struct ggml_tensor * d_last_src_attn_ln_q_w;
+    struct ggml_tensor * d_last_src_attn_ln_q_b;
+
+    // decoder.last_decoder.src_attn.linear_k_v.weight
+    struct ggml_tensor * d_last_src_attn_ln_kv_w;
+    struct ggml_tensor * d_last_src_attn_ln_kv_b;
+
+    // decoder.last_decoder.src_attn.linear_out.weight
+    struct ggml_tensor * d_last_src_attn_ln_o_w;
+    struct ggml_tensor * d_last_src_attn_ln_o_b;
+
+    // decoder.last_decoder.feed_forward.w_1.weight
+    struct ggml_tensor * d_last_mlp_ln_w1;
+    struct ggml_tensor * d_last_mlp_ln_b1;
+
+    // decoder.last_decoder.feed_forward.w_2.weight
+    struct ggml_tensor * d_last_mlp_ln_w2;
+
+    // decoder.last_decoder.feed_forward.norm.weight
+    struct ggml_tensor * d_last_mlp_norm_w;
+    struct ggml_tensor * d_last_mlp_norm_b;
+
+    // decoder.last_decoder.norm1.weight
+    struct ggml_tensor * d_last_norm_w1;
+    struct ggml_tensor * d_last_norm_b1;
+
+    //decoder.last_decoder.norm2.weight
+    struct ggml_tensor * d_last_norm_w2;
+    struct ggml_tensor * d_last_norm_b2;
+
+    //decoder.last_decoder.norm3.weight
+    struct ggml_tensor * d_last_norm_w3;
+    struct ggml_tensor * d_last_norm_b3;
+
+
 };
 
 
