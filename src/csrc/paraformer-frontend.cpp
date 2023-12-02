06c3181414d395fbbc97bbf0ef0da93d9090f88e
lovemefan <lovemefan@outlook.com>
2023-12-02 20:20:06 +0800

    add fbank + lfr + cmvn algorithm with multi-thread

--- src/csrc/paraformer-frontend.cpp
+++ src/csrc/paraformer-frontend.cpp
@@ -8,9 +8,11 @@
 static float sin_vals[SIN_COS_N_COUNT];
 static float cos_vals[SIN_COS_N_COUNT];
 
+#define M_2PI 6.283185307179586476925286766559005
+
 // In FFT, we frequently use sine and cosine operations with the same values.
 // We can use precalculated values to speed up the process.
-static void fill_sin_cos_table() {
+void fill_sin_cos_table() {
     static bool is_filled = false;
     if (is_filled) return;
     for (int i = 0; i < SIN_COS_N_COUNT; i++) {
@@ -19,6 +21,19 @@ static void fill_sin_cos_table() {
         cos_vals[i] = cosf(theta);
     }
     is_filled = true;
+}
+
+static void pre_emphasize(float *d, int32_t n, float pre_emph_coeff) {
+    if (pre_emph_coeff == 0.0) {
+        return;
+    }
+
+    assert(pre_emph_coeff >= 0.0 && pre_emph_coeff <= 1.0);
+
+    for (int32_t i = n - 1; i > 0; --i) {
+        d[i] -= pre_emph_coeff * d[i - 1];
+    }
+    d[0] -= pre_emph_coeff * d[0];
 }
 
 // naive Discrete Fourier Transform
@@ -49,6 +64,7 @@ static void dft(const std::vector<float> &in, std::vector<float> &out) {
 // poor man's implementation - use something better
 // input is real-valued
 // output is complex-valued
+// ref: // https://github.com/ggerganov/whisper.cpp/blob/master/whisper.cpp#L2373C1-L2429C1
 static void fft(const std::vector<float> &in, std::vector<float> &out) {
     out.resize(in.size() * 2);
 
@@ -102,7 +118,18 @@ static void fft(const std::vector<float> &in, std::vector<float> &out) {
     }
 }
 
-static bool hann_window(int length, bool periodic, std::vector<float> &output) {
+inline int32_t round_to_nearest_power_two(int32_t n) {
+    // copied from kaldi/src/base/kaldi-math.cc
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    return n + 1;
+}
+
+static bool hamming_window(int length, bool periodic, std::vector<float> &output) {
     if (output.size() < static_cast<size_t>(length)) {
         output.resize(length);
     }
@@ -111,65 +138,113 @@ static bool hann_window(int length, bool periodic, std::vector<float> &output) {
         offset = 0;
     }
     for (int i = 0; i < length; i++) {
-        output[i] = 0.5 * (1.0 - cosf((2.0 * M_PI * i) / (length + offset)));
+        output[i] = 0.54 - 0.46 * cosf((M_2PI * i) / (length + offset));
     }
 
     return true;
 }
 
-static void fbank_lfr_cmvn_feature_worker_thread(int ith, const std::vector<float> &hann,
-                                                 const std::vector<float> &samples, int n_samples, int frame_size,
-                                                 int frame_step, int n_threads, const paraformer_filters &filters,
-                                                 paraformer_mel &mel) {
-    std::vector<float> fft_in(frame_size, 0.0);
+static inline float inverse_mel_scale(float mel_freq) { return 700.0f * (expf(mel_freq / 1127.0f) - 1.0f); }
+
+static inline float mel_scale(float freq) { return 1127.0f * logf(1.0f + freq / 700.0f); }
+
+static void fbank_feature_worker_thread(int ith, const std::vector<float> &hamming, const std::vector<float> &samples,
+                                        int n_samples, int frame_size, int frame_step, int n_threads,
+                                        paraformer_mel &mel) {
     std::vector<float> fft_out(2 * frame_step);
     // make sure n_fft == 1 + (PARAFORMER_N_FFT / 2), bin_0 to bin_nyquist
     int n_fft = 1 + (frame_size / 2);
     int i = ith;
 
+    std::vector<float> window;
+    const int padded_window_size = round_to_nearest_power_two(frame_size);
+    window.resize(padded_window_size);
+
     // calculate FFT only when fft_in are not all zero
     for (; i < std::min(n_samples / frame_step + 1, mel.n_len); i += n_threads) {
         const int offset = i * frame_step;
 
-        // apply Hanning window (~10% faster)
-        for (int j = 0; j < std::min(frame_size, n_samples - offset); j++) {
-            fft_in[j] = hann[j] * samples[offset + j];
+        std::copy(samples.begin() + offset, samples.begin() + offset + frame_size, window.begin());
+
+        // remove dc offset
+        {
+            float sum = 0;
+            for (int32_t i = 0; i != frame_size; ++i) {
+                sum += window[i];
+            }
+            float mean = sum / frame_size;
+            for (int32_t i = 0; i != frame_size; ++i) {
+                window[i] -= mean;
+            }
         }
-        // fill the rest with zeros
-        if (n_samples - offset < frame_size) {
-            std::fill(fft_in.begin() + (n_samples - offset), fft_in.end(), 0.0);
+        // pre-emphasis
+        {
+            for (int32_t i = frame_size - 1; i > 0; --i) {
+                window[i] -= PREEMPH_COEFF * window[i - 1];
+            }
+            window[0] -= PREEMPH_COEFF * window[0];
+        }
+
+        // apply Hamming window
+        {
+            for (int j = 0; j < frame_size; j++) {
+                window[j] *= hamming[j];
+            }
+        }
+
+        // pad columns with zero
+        if (padded_window_size - frame_size > 0) {
+            std::fill(window.begin() + (padded_window_size - frame_size), window.end(), 0.0);
         }
 
         // FFT
-        fft(fft_in, fft_out);
+        fft(window, fft_out);
 
-        // Calculate modulus^2 of complex numbers
+        // Calculate modulus^2 of complex numbers，Power Spectrum
         // Use pow(fft_out[2 * j + 0], 2) + pow(fft_out[2 * j + 1], 2) causes inference quality problem? Interesting.
-        for (int j = 0; j < frame_size; j++) {
+        for (int j = 0; j < padded_window_size; j++) {
             fft_out[j] = (fft_out[2 * j + 0] * fft_out[2 * j + 0] + fft_out[2 * j + 1] * fft_out[2 * j + 1]);
         }
 
-        // mel spectrogram
-        for (int j = 0; j < mel.n_mel; j++) {
-            double sum = 0.0;
+        // log-Mel filter bank energies aka: "fbank"
+        {
+            auto num_fft_bins = padded_window_size / 2;
+            auto nyquist = 0.5 * PARAFORMER_SAMPLE_RATE;
+            auto low_freq = mel.low_freq;
+            auto high_freq = mel.high_freq;
+            auto vtln_high = mel.vtln_high;
+            auto vtln_low = mel.vtln_low;
 
-            // unroll loop (suggested by GH user @lunixbochs)
-            int k = 0;
-            for (k = 0; k < n_fft - 3; k += 4) {
-                sum += fft_out[k + 0] * filters.data[j * n_fft + k + 0] +
-                       fft_out[k + 1] * filters.data[j * n_fft + k + 1] +
-                       fft_out[k + 2] * filters.data[j * n_fft + k + 2] +
-                       fft_out[k + 3] * filters.data[j * n_fft + k + 3];
+            if (high_freq <= 0) high_freq += nyquist;
+
+            // fft-bin width [think of it as Nyquist-freq / half-window-length]
+            float fft_bin_width = PARAFORMER_SAMPLE_RATE * 1.0 / padded_window_size;
+            float mel_low_freq = mel_scale(low_freq);
+            float mel_high_freq = mel_scale(high_freq);
+
+            // divide by num_bins+1 in next line because of end-effects where the bins
+            // spread out to the sides.
+            auto mel_freq_delta = (mel_high_freq - mel_low_freq) / (mel.n_mel + 1);
+            if (vtln_high < 0.0f) vtln_high += nyquist;
+
+            for (int j = 0; j < mel.n_mel; j++) {
+                double sum = 0.0;
+                float left_mel = mel_low_freq + j * mel_freq_delta;
+                float center_mel = mel_low_freq + (j + 1.0) * mel_freq_delta;
+                float right_mel = mel_low_freq + (j + 2.0) * mel_freq_delta;
+
+                for (int k = 0; k < num_fft_bins; k++) {
+                    auto mel_num = mel_scale(fft_bin_width * k);
+                    auto up_slope = (mel_num - left_mel) / (center_mel - left_mel);
+                    auto down_slope = (right_mel - mel_num) / (right_mel - center_mel);
+                    auto filter = std::max(0.0f, std::min(up_slope, down_slope));
+                    sum += fft_out[k] * filter;
+                }
+
+                sum = log10(std::max(sum, 1e-10));
+
+                mel.data[j * mel.n_len + i] = sum;
             }
-
-            // handle n_fft remainder
-            for (; k < n_fft; k++) {
-                sum += fft_out[k] * filters.data[j * n_fft + k];
-            }
-
-            sum = log10(std::max(sum, 1e-10));
-
-            mel.data[j * mel.n_len + i] = sum;
         }
     }
 
@@ -182,53 +257,30 @@ static void fbank_lfr_cmvn_feature_worker_thread(int ith, const std::vector<floa
     }
 }
 
-// ref: https://github.com/ggerganov/ggml/blob/master/examples/whisper/whisper.cpp#L2833C24-L2833C24
-bool fbank_lfr_cmvn_feature(const float *samples, const int n_samples, const int frame_size, const int frame_step,
-                            const int n_mel, const int n_threads, paraformer_filters &filters, const bool debug,
+bool fbank_lfr_cmvn_feature(const std::vector<float> &samples, const int n_samples, const int frame_size,
+                            const int frame_step, const int n_mel, const int n_threads, const bool debug,
                             paraformer_mel &mel) {
     //    const int64_t t_start_us = ggml_time_us();
 
-    // Hanning window (Use cosf to eliminate difference)
-    // ref: https://pytorch.org/docs/stable/generated/torch.hann_window.html
-    // ref: https://github.com/openai/whisper/blob/main/paraformer/audio.py#L147
-    std::vector<float> hann;
-    hann_window(frame_size, true, hann);
-
-    // Calculate the length of padding
-    int64_t stage_1_pad = PARAFORMER_SAMPLE_RATE * 30;
-    int64_t stage_2_pad = frame_size / 2;
-
-    // Initialize a vector and copy data from C array to it.
-    std::vector<float> samples_padded;
-    samples_padded.resize(n_samples + stage_1_pad + stage_2_pad * 2);
-    std::copy(samples, samples + n_samples, samples_padded.begin() + stage_2_pad);
-
-    // pad 30 seconds of zeros at the end of audio (480,000 samples) + reflective pad 200 samples at the end of audio
-    std::fill(samples_padded.begin() + n_samples + stage_2_pad,
-              samples_padded.begin() + n_samples + stage_1_pad + 2 * stage_2_pad, 0);
-
-    // reflective pad 200 samples at the beginning of audio
-    std::reverse_copy(samples + 1, samples + 1 + stage_2_pad, samples_padded.begin());
-
+    const int32_t n_frames_per_ms = PARAFORMER_SAMPLE_RATE * 0.001f;
     mel.n_mel = n_mel;
-    // https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/native/SpectralOps.cpp#L936
-    // Calculate number of frames + remove the last frame
-    mel.n_len = (samples_padded.size() - frame_size) / frame_step;
-    // Calculate semi-padded sample length to ensure compatibility
-    mel.n_len_org = 1 + (n_samples + stage_2_pad - frame_size) / frame_step;
+    mel.n_len = 1 + ((n_samples - frame_size * n_frames_per_ms) / (frame_step * n_frames_per_ms));
     mel.data.resize(mel.n_mel * mel.n_len);
+
+    std::vector<float> hamming;
+    hamming_window(frame_size, true, hamming);
 
     {
         std::vector<std::thread> workers(n_threads - 1);
         for (int iw = 0; iw < n_threads - 1; ++iw) {
-            workers[iw] = std::thread(fbank_lfr_cmvn_feature_worker_thread, iw + 1, std::cref(hann), samples_padded,
-                                      n_samples + stage_2_pad, frame_size, frame_step, n_threads, std::cref(filters),
-                                      std::ref(mel));
+            workers[iw] =
+                std::thread(fbank_feature_worker_thread, iw + 1, std::cref(hamming), samples, n_samples,
+                            frame_size * n_frames_per_ms, frame_step * n_frames_per_ms, n_threads, std::ref(mel));
         }
 
         // main thread
-        fbank_lfr_cmvn_feature_worker_thread(0, hann, samples_padded, n_samples + stage_2_pad, frame_size, frame_step,
-                                             n_threads, filters, mel);
+        fbank_feature_worker_thread(0, hamming, samples, n_samples, frame_size * n_frames_per_ms,
+                                    frame_step * n_frames_per_ms, n_threads, mel);
 
         for (int iw = 0; iw < n_threads - 1; ++iw) {
             workers[iw].join();
@@ -266,5 +318,9 @@ bool fbank_lfr_cmvn_feature(const float *samples, const int n_samples, const int
         outFile.close();
     }
 
+    // todo  apply lrf, merge lfr_m frames as one,lfr_n frames per window
+    // ref: https://github.com/alibaba-damo-academy/FunASR/blob/main/runtime/onnxruntime/src/paraformer.cpp#L409-L440
+
+    // todo apply cvmn
     return true;
 }
--- src/csrc/paraformer-frontend.h
+++ src/csrc/paraformer-frontend.h
@@ -4,13 +4,17 @@
 #pragma once
 #include <fstream>
 #include <thread>
-
+#include <vector>
 #define PARAFORMER_SAMPLE_RATE 16000
+#define PREEMPH_COEFF 0.97
 struct paraformer_mel {
     int n_len;
     int n_len_org;
     int n_mel;
-
+    float low_freq = 20.0f;
+    float high_freq = 0.0f;
+    float vtln_high = -500.0f;
+    float vtln_low = 100.0f;
     std::vector<float> data;
 };
 
@@ -20,7 +24,7 @@ struct paraformer_filters {
 
     std::vector<float> data;
 };
-
-bool fbank_lfr_cmvn_feature(const float *samples, const int n_samples, const int frame_size, const int frame_step,
-                            const int n_mel, const int n_threads, paraformer_filters &filters, const bool debug,
+void fill_sin_cos_table();
+bool fbank_lfr_cmvn_feature(const std::vector<float> &samples, const int n_samples, const int frame_size,
+                            const int frame_step, const int n_mel, const int n_threads, const bool debug,
                             paraformer_mel &mel);
